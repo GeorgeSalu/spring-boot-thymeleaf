8016a11127428f7b4fe9521f93ca911bcfde9a2f
GeorgeSalu <george.salu10@gmail.com>
2024-08-20 19:10:46 -0300

    start do projeto basico-thymeleaf

---
+++ basico-thymeleaf/src/main/java/com/aulas/basico_thymeleaf/BasicoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.aulas.basico_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicoThymeleafApplication.class, args);
+	}
+
+}
