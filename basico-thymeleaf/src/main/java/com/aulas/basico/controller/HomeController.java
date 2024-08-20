ed188850e1c6886fe5fa98dc7cb4166f553b40c4
GeorgeSalu <george.salu10@gmail.com>
2024-08-20 19:46:31 -0300

    projeto basico-thymeleaf - primeiro controlador

---
+++ basico-thymeleaf/src/main/java/com/aulas/basico/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.aulas.basico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("msgBemVindo", "Bem-vindo a biblioteca");
+		return "publica-index";
+	}
+	
+}
