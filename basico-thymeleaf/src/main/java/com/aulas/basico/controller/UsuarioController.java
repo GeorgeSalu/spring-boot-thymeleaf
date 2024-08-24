ff96be585c2ecaa2a6fc98cda8771ce9045bb42e
GeorgeSalu <george.salu10@gmail.com>
2024-08-24 14:57:34 -0300

    projeto basico-thymeleaf - criacao de controlador de usuario e tela de adicionar usuario

---
+++ basico-thymeleaf/src/main/java/com/aulas/basico/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.aulas.basico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aulas.basico.modelo.Usuario;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+	@GetMapping("/novo")
+	public String adicionarUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "/publica-criar-usuario";
+	}
+	
+}
