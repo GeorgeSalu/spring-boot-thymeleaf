da03d74559bb0aee131acd8e80d2616a9fe7569c
GeorgeSalu <george.salu10@gmail.com>
2024-08-25 15:28:40 -0300

    projeto basico-thymeleaf - remove import nao utilizado

--- basico-thymeleaf/src/main/java/com/aulas/basico/controller/UsuarioController.java
+++ basico-thymeleaf/src/main/java/com/aulas/basico/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
