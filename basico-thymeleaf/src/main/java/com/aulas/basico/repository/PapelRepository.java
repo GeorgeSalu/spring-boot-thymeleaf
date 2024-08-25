a295a03478056834ce9e1deb89efc7d2b6bd5316
GeorgeSalu <george.salu10@gmail.com>
2024-08-25 16:09:49 -0300

    projeto basico-thymeleaf - criando repository da entidade papel

---
+++ basico-thymeleaf/src/main/java/com/aulas/basico/repository/PapelRepository.java
@@ -0,0 +1,9 @@
+package com.aulas.basico.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aulas.basico.modelo.Papel;
+
+public interface PapelRepository extends JpaRepository<Papel, Long> {
+
+}
