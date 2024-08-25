3bc55a46da751893168263e9d866ada53ac1d479
GeorgeSalu <george.salu10@gmail.com>
2024-08-25 16:41:36 -0300

    projeto basico-thymeleaf - usando a anotacao GetMapping em homeController

--- basico-thymeleaf/src/main/java/com/aulas/basico/controller/HomeController.java
+++ basico-thymeleaf/src/main/java/com/aulas/basico/controller/HomeController.java
@@ -2,12 +2,12 @@ package com.aulas.basico.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("msgBemVindo", "Bem-vindo à biblioteca");
 		return "publica-index";
