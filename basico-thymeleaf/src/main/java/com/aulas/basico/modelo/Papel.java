3c2d78555239619dcc3d99d551b69746aab7a06e
GeorgeSalu <george.salu10@gmail.com>
2024-08-25 15:58:52 -0300

    projeto basico-thymeleaf - criando entidade papel e relacionando com entidade usuario

---
+++ basico-thymeleaf/src/main/java/com/aulas/basico/modelo/Papel.java
@@ -0,0 +1,39 @@
+package com.aulas.basico.modelo;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Papel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String papel;
+	
+	@ManyToMany(mappedBy = "papeis", fetch = FetchType.EAGER)
+	private List<Usuario> usuarios;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPapel() {
+		return papel;
+	}
+
+	public void setPapel(String papel) {
+		this.papel = papel;
+	}
+
+}
