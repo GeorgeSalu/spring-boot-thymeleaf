f501312b6fb5705306ab92a1565b6ddf2bbc5827
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 15:37:52 -0300

    start projeto basico-crud-thymeleaf

---
+++ basico-crud-thymeleaf/src/main/java/com/example/basico_crud_thymeleaf/BasicoCrudThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.basico_crud_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicoCrudThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicoCrudThymeleafApplication.class, args);
+	}
+
+}
---
+++ basico-crud-thymeleaf/src/test/java/com/example/basico_crud_thymeleaf/BasicoCrudThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basico_crud_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicoCrudThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
