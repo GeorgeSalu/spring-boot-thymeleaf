413cad58653faf815d80f1764543f8a63751ab65
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 15:45:35 -0300

    projeto basico-crud-thymeleaf - definindo classe Startup da appliacacao

--- basico-crud-thymeleaf/src/main/java/com/example/crud/ApplicationStartup.java
+++ basico-crud-thymeleaf/src/main/java/com/example/crud/ApplicationStartup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicoCrudThymeleafApplication {
+public class ApplicationStartup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicoCrudThymeleafApplication.class, args);
+		SpringApplication.run(ApplicationStartup.class, args);
 	}
 
 }
