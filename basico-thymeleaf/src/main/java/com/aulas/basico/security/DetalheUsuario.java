89be8f2ea725ea7a2e9e728928e2318f5610f25e
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 14:51:07 -0300

    projeto basico-thymeleaf - implementa serialVersionUID aleatorio em DetalheUsuario

--- basico-thymeleaf/src/main/java/com/aulas/basico/security/DetalheUsuario.java
+++ basico-thymeleaf/src/main/java/com/aulas/basico/security/DetalheUsuario.java
@@ -13,7 +13,7 @@ import com.aulas.basico.modelo.Usuario;
 
 public class DetalheUsuario implements UserDetails {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -408853313369003841L;
 	private Usuario usuario;
 	
 	public DetalheUsuario(Usuario usuario) {
