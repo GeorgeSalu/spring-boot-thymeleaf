0d1e9dfcd1260162fbbd41fd84ed195c6e10f4ed
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 14:46:04 -0300

    projeto basico-thymeleaf - remove import nao utilizados em DetalheUsuarioService

--- basico-thymeleaf/src/main/java/com/aulas/basico/security/DetalheUsuarioServico.java
+++ basico-thymeleaf/src/main/java/com/aulas/basico/security/DetalheUsuarioServico.java
@@ -1,19 +1,12 @@
 package com.aulas.basico.security;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.transaction.Transactional;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.aulas.basico.modelo.Papel;
 import com.aulas.basico.modelo.Usuario;
 import com.aulas.basico.repository.UsuarioRepository;
 
