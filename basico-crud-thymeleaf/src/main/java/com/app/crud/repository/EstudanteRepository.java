f2991c9274f8b6b9835db6ee132c517411059de7
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 22:11:19 -0300

    projeto basico-crud-thymeleaf - criando a camada de repositorio

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/repository/EstudanteRepository.java
@@ -0,0 +1,9 @@
+package com.app.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.crud.modelo.Estudante;
+
+public interface EstudanteRepository extends JpaRepository<Estudante, Long>{
+
+}
