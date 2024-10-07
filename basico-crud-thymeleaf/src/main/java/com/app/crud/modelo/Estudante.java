5192bb9a091cbfa140e62084961510388d577d37
GeorgeSalu <george.salu10@gmail.com>
2024-10-06 22:04:34 -0300

    projeto basico-crud-thymeleaf - criando toString na entidade Estudante

--- basico-crud-thymeleaf/src/main/java/com/app/crud/modelo/Estudante.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/modelo/Estudante.java
@@ -46,4 +46,9 @@ public class Estudante {
 		this.idade = idade;
 	}
 
+	@Override
+	public String toString() {
+		return "Estudante [id=" + id + ", nome=" + nome + ", idade=" + idade + "]";
+	}
+
 }
