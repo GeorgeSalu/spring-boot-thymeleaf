dbf3058f229273825c9747b3b6fe36d67f12ac55
GeorgeSalu <george.salu10@gmail.com>
2024-10-18 19:49:44 -0300

    projeto basico-crud-thymeleaf - cria rota de lista-estudantes

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/EstudanteController.java
@@ -0,0 +1,14 @@
+package com.app.crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EstudanteController {
+
+	@GetMapping("/")
+	public String listarEstudantes() {
+		return "/lista-estudantes";
+	}
+	
+}
