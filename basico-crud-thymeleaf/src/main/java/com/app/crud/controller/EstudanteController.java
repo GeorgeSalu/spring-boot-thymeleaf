a137126bd72bd0ff97bc5e42280688fb248277d2
GeorgeSalu <george.salu10@gmail.com>
2024-10-19 20:24:12 -0300

    projeto basico-crud-thymeleaf - validando dados no cadastro de estudante

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/EstudanteController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/EstudanteController.java
@@ -3,6 +3,7 @@ package com.app.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.app.crud.modelo.Estudante;
 import com.app.crud.service.EstudanteService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class EstudanteController {
@@ -30,7 +33,13 @@ public class EstudanteController {
 	}
 	
 	@PostMapping("/gravar")
-	public String gravarEstudante(@ModelAttribute("novoEstudante") Estudante estudante,RedirectAttributes attributes) {
+	public String gravarEstudante(@ModelAttribute("novoEstudante") @Valid Estudante estudante,
+								  BindingResult bindingResult,RedirectAttributes attributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return "/novo-estudante";
+		}
+		
 		estudanteService.criarEstudante(estudante);
 		attributes.addFlashAttribute("mensagem", "Estudante salvo com sucesso!");
 		return "redirect:/novo";
