19af7a105ef59838ec713e35d9c6d3610fb3fd0c
GeorgeSalu <george.salu10@gmail.com>
2024-10-19 19:55:15 -0300

    projeto basico-crud-thymeleaf - gravando objetos de uma entidade no bando de dados

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/EstudanteController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/EstudanteController.java
@@ -1,10 +1,20 @@
 package com.app.crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.crud.modelo.Estudante;
+import com.app.crud.service.EstudanteService;
 
 @Controller
 public class EstudanteController {
+	
+	@Autowired
+	private EstudanteService estudanteService;
 
 	@GetMapping("/")
 	public String listarEstudantes() {
@@ -12,8 +22,16 @@ public class EstudanteController {
 	}
 	
 	@GetMapping("/novo")
-	public String novoEstudante() {
+	public String novoEstudante(Model model) {
+		Estudante estudante = new Estudante();
+		model.addAttribute("novoEstudante", estudante);
 		return "/novo-estudante";
+	}
+	
+	@PostMapping("/gravar")
+	public String gracar(@ModelAttribute("novoEstudante") Estudante estudante) {
+		estudanteService.criarEstudante(estudante);
+		return "redirect:/novo";
 	}
 	
 }
