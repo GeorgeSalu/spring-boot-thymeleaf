dfe7842c95dd6d241e8ab11f499834f4d0db2af4
GeorgeSalu <george.salu10@gmail.com>
2024-10-19 21:46:14 -0300

    projeto basico-crud-thymeleaf - adiciona serialVersionId em EstudanteNotFoundException

--- basico-crud-thymeleaf/src/main/java/com/app/crud/exception/EstudanteNotFoundException.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/exception/EstudanteNotFoundException.java
@@ -2,6 +2,8 @@ package com.app.crud.exception;
 
 public class EstudanteNotFoundException extends Exception {
 
+	private static final long serialVersionUID = -7468834076450971679L;
+
 	public EstudanteNotFoundException(String message) {
 		super(message);
 	}
