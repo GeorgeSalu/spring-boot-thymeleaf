388e8afe95f05588f11f5d25fe0a4f5e79865515
GeorgeSalu <george.salu10@gmail.com>
2024-10-22 20:12:00 -0300

    projeto basico-crud-thymeleaf - criando o repository da entidade de endereco

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.app.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.crud.modelo.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
