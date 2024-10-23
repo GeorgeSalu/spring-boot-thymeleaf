d2f0cb2adc01737b6718aec94a2b3fa4b21377f5
GeorgeSalu <george.salu10@gmail.com>
2024-10-22 22:35:01 -0300

    projeto basico-crud-thymeleaf - criando a entidade de estudante

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/modelo/Area.java
@@ -0,0 +1,48 @@
+package com.app.crud.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Area {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String descricao;
+	private List<Estudante> estudantes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Estudante> getEstudantes() {
+		return estudantes;
+	}
+
+	public void setEstudantes(List<Estudante> estudantes) {
+		this.estudantes = estudantes;
+	}
+
+	@Override
+	public String toString() {
+		return "Area [id=" + id + ", descricao=" + descricao + ", estudantes=" + estudantes + "]";
+	}
+
+}
