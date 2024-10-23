d80eced7225eff880fd0e09c3cc8ddfb6ebf967a
GeorgeSalu <george.salu10@gmail.com>
2024-10-23 19:22:27 -0300

    projeto basico-crud-thymeleaf - fluxo para abrir formulario de cadastro de area

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/AreaController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/AreaController.java
@@ -2,13 +2,25 @@ package com.app.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.crud.modelo.Area;
 import com.app.crud.service.AreaService;
 
 @Controller
+@RequestMapping("/area")
 public class AreaController {
 
 	@Autowired
 	private AreaService areaService;
+
+	@GetMapping("/novo")
+	public String novoArea(Model model) {
+		Area area = new Area();
+		model.addAttribute("novoArea", area);
+		return "novo-area";
+	}
 	
 }
