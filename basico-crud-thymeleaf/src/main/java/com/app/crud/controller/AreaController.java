30ecf7259964cb42d23a0d7a968bd71c11beb3f5
GeorgeSalu <george.salu10@gmail.com>
2024-10-23 19:29:45 -0300

    projeto basico-crud-thymeleaf - fluxo  de cadastro de area

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/AreaController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/AreaController.java
@@ -3,11 +3,17 @@ package com.app.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.app.crud.modelo.Area;
 import com.app.crud.service.AreaService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/area")
@@ -23,4 +29,23 @@ public class AreaController {
 		return "novo-area";
 	}
 	
+	@PostMapping("/gravar")
+	public String gravarArea(@ModelAttribute("novoArea") @Valid Area area,
+							 BindingResult erros,
+							 RedirectAttributes attributes) {
+		
+		if (erros.hasErrors()) {
+			return "novo-area";
+		}
+		areaService.gravar(area);
+		attributes.addFlashAttribute("mensagem", "Area salva com sucesso");
+		return "redirect:/area/novo";
+	}
+	
 }
+
+
+
+
+
+
