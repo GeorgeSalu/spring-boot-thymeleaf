387baaf5586cce87182f21eba86734438b03e941
GeorgeSalu <george.salu10@gmail.com>
2024-10-23 21:52:00 -0300

    projeto basico-crud-thymeleaf - incluindo validacao na entidade Habilidade

--- basico-crud-thymeleaf/src/main/java/com/app/crud/modelo/Habilidade.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/modelo/Habilidade.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Habilidade {
@@ -12,6 +14,8 @@ public class Habilidade {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "O descrição deve ser informado")
+	@Size(min = 2, message = "O descrição deve ter no mínimo 2 caracteres")
 	private String nome;
 
 	public Long getId() {
