9ebebdf41cadccc5c516cf31219979c1ca7fffd4
GeorgeSalu <george.salu10@gmail.com>
2024-10-23 22:11:44 -0300

    projeto basico-crud-thymeleaf - criando a camada de repositorio da entidade Habilidade

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/repository/HabilidadeRepositorio.java
@@ -0,0 +1,9 @@
+package com.app.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.crud.modelo.Habilidade;
+
+public interface HabilidadeRepositorio extends JpaRepository<Habilidade, Long>{
+
+}
