ddc2bcb6552d1ca8381c99d9d6764f998d526a3c
GeorgeSalu <george.salu10@gmail.com>
2024-10-23 22:18:26 -0300

    projeto basico-crud-thymeleaf - criando a camada de service da entidade Habilidade

---
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/service/HabilidadeService.java
@@ -0,0 +1,19 @@
+package com.app.crud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.crud.modelo.Habilidade;
+import com.app.crud.repository.HabilidadeRepositorio;
+
+@Service
+public class HabilidadeService {
+
+	@Autowired
+	private HabilidadeRepositorio habilidadeRepositorio;
+	
+	public Habilidade criarHabilidade(Habilidade habilidade) {
+		return habilidadeRepositorio.save(habilidade);
+	}
+	
+}
