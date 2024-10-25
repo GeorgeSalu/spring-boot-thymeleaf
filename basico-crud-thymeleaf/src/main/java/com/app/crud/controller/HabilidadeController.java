096a9950f65a604f9b115fab87be7fd23e800484
GeorgeSalu <george.salu10@gmail.com>
2024-10-25 20:45:39 -0300

    projeto basico-crud-thymeleaf - criando pagina de cadastro de Habilidade

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/HabilidadeController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/HabilidadeController.java
@@ -2,8 +2,11 @@ package com.app.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.crud.modelo.Habilidade;
 import com.app.crud.service.HabilidadeService;
 
 @Controller
@@ -12,5 +15,12 @@ public class HabilidadeController {
 
 	@Autowired
 	private HabilidadeService habilidadeService;
+	
+	@GetMapping("/novo")
+	public String novahabilidade(Model model) {
+		Habilidade habilidade = new Habilidade();
+		model.addAttribute("item", habilidade);
+		return "/novo-habilidade";
+	}
 	
 }
