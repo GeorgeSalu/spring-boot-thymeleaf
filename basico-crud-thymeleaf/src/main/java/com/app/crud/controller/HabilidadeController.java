a95d4010f0848d80e40e97d0cb1165d99af603e6
GeorgeSalu <george.salu10@gmail.com>
2024-10-25 20:54:25 -0300

    projeto basico-crud-thymeleaf - implementa fluxo de gravacao de habilidaes

--- basico-crud-thymeleaf/src/main/java/com/app/crud/controller/HabilidadeController.java
+++ basico-crud-thymeleaf/src/main/java/com/app/crud/controller/HabilidadeController.java
@@ -3,11 +3,17 @@ package com.app.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.app.crud.modelo.Habilidade;
 import com.app.crud.service.HabilidadeService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/habilidade")
@@ -21,6 +27,18 @@ public class HabilidadeController {
 		Habilidade habilidade = new Habilidade();
 		model.addAttribute("item", habilidade);
 		return "/novo-habilidade";
+	}
+	
+	@PostMapping("/gravar")
+	public String gravarHabilidade(@ModelAttribute("item") @Valid Habilidade habilidade,
+								   BindingResult erros, RedirectAttributes attributes) {
+	
+		if(erros.hasErrors()) {
+			return "/novo-habilidade";
+		}
+		habilidadeService.criarHabilidade(habilidade);
+		attributes.addFlashAttribute("mensagem", "Habilidade salva com sucesso !");
+		return "redirect:/habilidade/novo";
 	}
 	
 }
