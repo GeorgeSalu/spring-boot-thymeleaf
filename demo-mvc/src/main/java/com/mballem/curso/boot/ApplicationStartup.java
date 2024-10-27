489f682a329c457226307cfb975851f4ace5ab41
GeorgeSalu <george.salu10@gmail.com>
2024-10-27 09:13:52 -0300

    projeto demo-mvc - renomeia classe de inicio da applicacao

--- demo-mvc/src/main/java/com/mballem/curso/boot/ApplicationStartup.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/ApplicationStartup.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoMvcApplication {
+public class ApplicationStartup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoMvcApplication.class, args);
+		SpringApplication.run(ApplicationStartup.class, args);
 	}
 }
