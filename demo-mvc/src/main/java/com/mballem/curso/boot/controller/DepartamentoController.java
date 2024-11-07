616a544c1f67a89313984f4fc3a6025c746e13f7
GeorgeSalu <george.salu10@gmail.com>
2024-11-07 20:29:12 -0300

    projeto demo-mvc - excluindo departamentos

--- demo-mvc/src/main/java/com/mballem/curso/boot/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/controller/DepartamentoController.java
@@ -11,44 +11,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.mballem.curso.boot.domain.Departamento;
 import com.mballem.curso.boot.service.DepartamentoService;
 
-
-
 @Controller
 @RequestMapping("departamentos")
 public class DepartamentoController {
 
 	@Autowired
 	private DepartamentoService service;
-	
+
 	@GetMapping("/cadastrar")
 	public String cadastro(Departamento departamento) {
 		return "departamento/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
 		model.addAttribute("departamentos", service.buscarTodos());
 		return "departamento/lista";
 	}
-	
+
 	@PostMapping("/salvar")
 	public String salvar(Departamento departamento) {
 		service.salvar(departamento);
 		return "redirect:/departamentos/cadastrar";
 	}
-	
+
 	@GetMapping("/editar/{id}")
-	public String preEditar(@PathVariable("id") Long id,ModelMap model) {
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
 		model.addAttribute("departamento", service.buscarPorId(id));
 		return "/departamento/cadastro";
 	}
-	
+
 	@PostMapping("/editar")
 	public String editar(Departamento entity) {
 		service.editar(entity);
 		return "redirect:/departamentos/cadastrar";
 	}
-	
-	
-	
+
+	@GetMapping("/excluir/{id}")
+	public String getMethodName(@PathVariable("id") Long id, ModelMap model) {
+		if (!service.departamentoTemCargos(id)) {
+			service.excluir(id);
+		}
+		return listar(model);
+	}
+
 }
