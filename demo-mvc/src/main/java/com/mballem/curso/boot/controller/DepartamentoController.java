3eb5f9f886bf83f3f6e0d63256a4e29e0a15cc72
GeorgeSalu <george.salu10@gmail.com>
2024-11-07 19:25:33 -0300

    projeto demo-mvc - inserindo departamentos

--- demo-mvc/src/main/java/com/mballem/curso/boot/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/controller/DepartamentoController.java
@@ -1,21 +1,35 @@
 package com.mballem.curso.boot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mballem.curso.boot.domain.Departamento;
+import com.mballem.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastro() {
+	public String cadastro(Departamento departamento) {
 		return "departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 	
 }
