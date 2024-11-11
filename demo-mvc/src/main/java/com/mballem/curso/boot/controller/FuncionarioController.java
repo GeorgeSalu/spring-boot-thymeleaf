f1676e3a7adbfc1450f78675647d7eb0087dd204
GeorgeSalu <george.salu10@gmail.com>
2024-11-11 20:21:06 -0300

    projeto demo-mvc - configurando a pagina de cadastro de funcionario com a classe FuncionarioController

--- demo-mvc/src/main/java/com/mballem/curso/boot/controller/FuncionarioController.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/controller/FuncionarioController.java
@@ -1,15 +1,33 @@
 package com.mballem.curso.boot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.domain.Funcionario;
+import com.mballem.curso.boot.domain.UF;
+import com.mballem.curso.boot.service.CargoService;
+import com.mballem.curso.boot.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private CargoService cargoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 	
@@ -18,4 +36,25 @@ public class FuncionarioController {
 		return "/funcionario/lista";
 	}
 	
+	@PostMapping("salvar")
+	public String salvar(Funcionario funcionario,RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "funcioario inserido com sucesso");
+		return "redirect:/funcionario/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
+	}
+	
 }
+
+
+
+
