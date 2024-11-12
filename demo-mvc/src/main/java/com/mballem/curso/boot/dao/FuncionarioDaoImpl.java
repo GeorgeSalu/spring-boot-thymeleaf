4f540a202853adbecfd9f7f2d634c8168fe7a476
GeorgeSalu <george.salu10@gmail.com>
2024-11-11 21:47:58 -0300

    projeto demo-mvc - busca funcionarios por nome

--- demo-mvc/src/main/java/com/mballem/curso/boot/dao/FuncionarioDaoImpl.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/dao/FuncionarioDaoImpl.java
@@ -1,5 +1,7 @@
 package com.mballem.curso.boot.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.mballem.curso.boot.domain.Funcionario;
@@ -7,4 +9,10 @@ import com.mballem.curso.boot.domain.Funcionario;
 @Repository
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao {
 
+	public List<Funcionario> findByNome(String nome) {
+		
+		return createQuery("select f from Funcionario f where f.nome like concat('%', ?1 ,'%') ", nome);
+		
+	}
+	
 }
