986823762d8493c1158572c2f3410785e15a81bc
GeorgeSalu <george.salu10@gmail.com>
2024-11-11 22:09:07 -0300

    projeto demo-mvc - busca funcionarios por cargo

--- demo-mvc/src/main/java/com/mballem/curso/boot/service/FuncionarioService.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/service/FuncionarioService.java
@@ -12,5 +12,6 @@ public interface FuncionarioService {
 	Funcionario buscarPorId(Long id);
 	List<Funcionario> buscarTodos();
 	List<Funcionario> buscarPorNome(String nome);
+	List<Funcionario> buscarPorCargo(Long id);
 	
 }
