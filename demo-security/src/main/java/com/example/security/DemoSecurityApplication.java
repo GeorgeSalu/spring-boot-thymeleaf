93252478e33bc09949a63110691f599feb1dc865
George Salu <george.salu10@gmail.com>
2024-12-30 16:17:26 -0300

    demo-security - definindo o pacote raiz

--- demo-security/src/main/java/com/example/security/DemoSecurityApplication.java
+++ demo-security/src/main/java/com/example/security/DemoSecurityApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo_security;
+package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
