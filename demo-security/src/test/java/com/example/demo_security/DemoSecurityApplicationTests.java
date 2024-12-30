ce66d1a838b6529353517a257384db581f35b353
George Salu <george.salu10@gmail.com>
2024-12-30 16:13:08 -0300

    cria projeto demo-security

---
+++ demo-security/src/test/java/com/example/demo_security/DemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_security;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
