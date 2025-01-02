0731505bff25d5efb6f2bf88f74addf53a78aaeb
George Salu <george.salu10@gmail.com>
2025-01-02 11:48:03 -0300

    demo-security - lidando com login e logout

--- demo-security/src/main/java/com/example/security/controller/HomeController.java
+++ demo-security/src/main/java/com/example/security/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,20 @@ public class HomeController {
 	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
-	}	
+	}
+	
+	@GetMapping({"/login"})
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais invalidas");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadstro ja ativados");
+		
+		return "login";
+	}
 }
