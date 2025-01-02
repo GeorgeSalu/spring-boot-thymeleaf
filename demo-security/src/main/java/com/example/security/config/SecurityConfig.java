e4cf5650c1219ef6bde9dba32f8e274137fbc91b
George Salu <george.salu10@gmail.com>
2025-01-02 11:09:54 -0300

    demo-security - liberando acesso publico a home

--- demo-security/src/main/java/com/example/security/config/SecurityConfig.java
+++ demo-security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers("/").permitAll()
+			.antMatchers("/", "/home").permitAll()
 			.anyRequest().authenticated();
 	}
 	
