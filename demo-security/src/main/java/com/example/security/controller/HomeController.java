b1250d2184e9d1b72967f2e0d723dcf5de1cec5b
George Salu <george.salu10@gmail.com>
2025-01-03 12:29:33 -0300

    demo-security - tratando o acesso negado

--- demo-security/src/main/java/com/example/security/controller/HomeController.java
+++ demo-security/src/main/java/com/example/security/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.security.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +25,14 @@ public class HomeController {
 		model.addAttribute("titulo", "Credenciais invalidas");
 		model.addAttribute("texto", "Login ou senha incorretos, tente novamente");
 		model.addAttribute("subtexto", "Acesso permitido apenas para cadstro ja ativados");
-		
 		return "login";
+	}
+	
+	@GetMapping({"/acesso-negado"})
+	public String acessoNegado(ModelMap model, HttpServletResponse resp) {
+		model.addAttribute("status", resp.getStatus());
+		model.addAttribute("error", "Acesso Negado");
+		model.addAttribute("message", "Voce não tem permissao para acesso a esta area ou ação");
+		return "error";
 	}
 }
