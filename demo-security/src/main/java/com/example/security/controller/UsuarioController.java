f38e41794f0a493587a200cc8c40195905aab19d
George Salu <george.salu10@gmail.com>
2025-01-04 16:39:20 -0300

    demo-security - cadastro de novos usuarios

--- demo-security/src/main/java/com/example/security/controller/UsuarioController.java
+++ demo-security/src/main/java/com/example/security/controller/UsuarioController.java
@@ -1,13 +1,19 @@
 package com.example.security.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.security.domain.Perfil;
 import com.example.security.domain.Usuario;
 import com.example.security.service.UsuarioService;
 
@@ -34,6 +40,25 @@ public class UsuarioController {
 	public ResponseEntity<?> listarUsuarioDatatables(HttpServletRequest request) {
 		
 		return ResponseEntity.ok(service.buscarTodos(request));
+	}
+	
+	@PostMapping("/cadastro/salvar")
+	public String salvarUsuarios(Usuario usuario,RedirectAttributes attr) {
+		
+		List<Perfil> perfis = usuario.getPerfis();
+		
+		if(perfis.size() > 2 || perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) || 
+				perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
+			
+			attr.addFlashAttribute("falha", "Paciente nao pode ser Admin e ou Medico");
+			attr.addFlashAttribute("usuario", usuario);
+			
+		} else {
+			service.salvarUsuario(usuario);
+			attr.addFlashAttribute("sucesso", "Operacao realizada com sucesso");
+		}
+		
+		return "redirect:/u/novo/cadastro/usuario";
 	}
 	
 }
