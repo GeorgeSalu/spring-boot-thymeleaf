b2232f90e7127bf47de71b89cb6318fe25c5991c
George Salu <george.salu10@gmail.com>
2025-01-04 14:10:08 -0300

    demo-security - processo para listagem de usuarios cadastrados

--- demo-security/src/main/java/com/example/security/controller/UsuarioController.java
+++ demo-security/src/main/java/com/example/security/controller/UsuarioController.java
@@ -1,19 +1,39 @@
 package com.example.security.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.security.domain.Usuario;
+import com.example.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService service;
 
 	@GetMapping("/novo/cadastro/usuario")
 	public String cadastroPorAdminParaAdminMedicoPaciente(Usuario usuario) {
 		
 		return "usuario/cadastro";
+	}
+
+	@GetMapping("/lista")
+	public String listartUsuarios() {
+		
+		return "usuario/lista";
+	}
+	
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuarioDatatables(HttpServletRequest request) {
+		
+		return ResponseEntity.ok(service.buscarTodos(request));
 	}
 	
 }
