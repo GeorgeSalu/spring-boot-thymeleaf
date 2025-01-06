8674cac7d49af88b9a2054dffa7c4a7874218516
George Salu <george.salu10@gmail.com>
2025-01-06 12:05:48 -0300

    demo-security - metodos salvar e editar para medicos

--- demo-security/src/main/java/com/example/security/controller/MedicoController.java
+++ demo-security/src/main/java/com/example/security/controller/MedicoController.java
@@ -1,20 +1,46 @@
 package com.example.security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.security.domain.Medico;
+import com.example.security.service.MedicoService;
 
 @Controller
 @RequestMapping("medicos")
 public class MedicoController {
+	
+	@Autowired
+	private MedicoService medicoService;
 
 	@GetMapping({"/dados"})
 	public String abrirPorMedico(Medico medico,ModelMap model) {
 		
 		return "medico/cadastro";
+	}
+	
+	@GetMapping({"/salvar"})
+	public String salvar(Medico medico,RedirectAttributes attr) {
+		
+		medicoService.salvar(medico);
+		attr.addFlashAttribute("sucesso", "Operacao realizada com sucesso");
+		attr.addFlashAttribute("medico", medico);
+		
+		return "redirect:/medicos/dados";
+	}
+	
+	@GetMapping({"/editar"})
+	public String editar(Medico medico,RedirectAttributes attr) {
+		
+		medicoService.editar(medico);
+		attr.addFlashAttribute("sucesso", "Operacao realizada com sucesso");
+		attr.addFlashAttribute("medico", medico);
+		
+		return "redirect:/medicos/dados";
 	}
 	
 }
--- demo-security/src/main/java/com/example/security/service/MedicoService.java
+++ demo-security/src/main/java/com/example/security/service/MedicoService.java
@@ -19,5 +19,26 @@ public class MedicoService {
 		return repository.findByUsuarioId(id)
 					.orElse(new Medico());
 	}
+
+	@Transactional(readOnly = true)
+	public void salvar(Medico medico) {
+		
+		repository.save(medico);
+	}
+
+	@Transactional(readOnly = true)
+	public void editar(Medico medico) {
+
+		Medico m2 = repository.findById(medico.getId()).get();
+		m2.setCrm(medico.getCrm());
+		m2.setDtInscricao(medico.getDtInscricao());
+		m2.setNome(medico.getNome());
+		
+		if(!medico.getEspecialidades().isEmpty()) {
+			
+			m2.getEspecialidades().addAll(medico.getEspecialidades());
+		}
+		
+	}
 	
 }
