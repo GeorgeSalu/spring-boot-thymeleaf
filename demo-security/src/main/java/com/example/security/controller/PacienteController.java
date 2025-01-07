149acfd2524b1abbc7f8701cef481acd327656d4
George Salu <george.salu10@gmail.com>
2025-01-07 13:50:29 -0300

    demo-security - inserindo os dados pessoais do paciente

--- demo-security/src/main/java/com/example/security/controller/PacienteController.java
+++ demo-security/src/main/java/com/example/security/controller/PacienteController.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.security.domain.Paciente;
 import com.example.security.domain.Usuario;
 import com.example.security.service.PacienteService;
+import com.example.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("pacientes")
@@ -18,6 +20,9 @@ public class PacienteController {
 
 	@Autowired
 	private PacienteService service;
+	
+	@Autowired
+	private UsuarioService usuarioService;
 	
 	@GetMapping("/dados")
 	public String cadastrar(Paciente paciente,ModelMap model,@AuthenticationPrincipal User user) {
@@ -33,4 +38,44 @@ public class PacienteController {
 		return "paciente/cadastro";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Paciente paciente,ModelMap model,@AuthenticationPrincipal User user) {
+		
+		Usuario u = usuarioService.buscarPorEmail(user.getUsername());
+		
+		if(UsuarioService.isSenhaCorreta(paciente.getUsuario().getSenha(), u.getSenha())) {
+			
+			paciente.setUsuario(u);
+			
+			service.salvar(paciente);
+			model.addAttribute("sucesso", "Seus dados foram inseridos com sucesso");
+			
+		} else {
+			model.addAttribute("falha", "Sua senha nao confere, tente novamente");
+		}
+		
+		
+		return "paciente/cadastro";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
