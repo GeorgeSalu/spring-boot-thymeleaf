35f5c2bfe1bd6ea09051730a3cda5a70e1181ff3
George Salu <george.salu10@gmail.com>
2025-01-08 14:39:03 -0300

    demo-security - renomeia variavel de service em PacienteService

--- demo-security/src/main/java/com/example/security/service/PacienteService.java
+++ demo-security/src/main/java/com/example/security/service/PacienteService.java
@@ -11,24 +11,24 @@ import com.example.security.repository.PacienteRepository;
 public class PacienteService {
 
 	@Autowired
-	private PacienteRepository repository;
+	private PacienteRepository pacienteRepository;
 	
 	@Transactional(readOnly = true)
 	public Paciente buscarPorUsuarioEmail(String email) {
 		
-		return repository.findByUsuarioEmail(email).orElse(new Paciente());
+		return pacienteRepository.findByUsuarioEmail(email).orElse(new Paciente());
 	}
 
 	@Transactional(readOnly = false)
 	public void salvar(Paciente paciente) {
 
-		repository.save(paciente);
+		pacienteRepository.save(paciente);
 	}
 
 	@Transactional(readOnly = false)
 	public void editar(Paciente paciente) {
 		
-		Paciente p2 = repository.findById(paciente.getId()).get();
+		Paciente p2 = pacienteRepository.findById(paciente.getId()).get();
 		p2.setNome(paciente.getNome());
 		p2.setDtNascimento(paciente.getDtNascimento());
 		
