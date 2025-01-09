cc342703960e04c83545f8712976c939ed9d3365
George Salu <george.salu10@gmail.com>
2025-01-09 20:04:08 -0300

    criando o projeto demo-freemarker

---
+++ demo-freemarker/src/main/java/com/example/demo_freemarker/DemoFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFreemarkerApplication.class, args);
+	}
+
+}
---
+++ demo-freemarker/src/test/java/com/example/demo_freemarker/DemoFreemarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_freemarker;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFreemarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
