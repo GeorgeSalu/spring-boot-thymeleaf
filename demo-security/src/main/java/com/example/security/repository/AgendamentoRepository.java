b0c256da6e76bb2635048a7dad1cc61d510c9fb0
George Salu <george.salu10@gmail.com>
2025-01-08 23:13:10 -0300

    demo-security - usando a annotation @repository do spring em agendamentoRepository

--- demo-security/src/main/java/com/example/security/repository/AgendamentoRepository.java
+++ demo-security/src/main/java/com/example/security/repository/AgendamentoRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.security.domain.Agendamento;
 import com.example.security.domain.Horario;
 import com.example.security.repository.projection.HistoricoPaciente;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
 	@Query("select h from Horario h where not exists( "
