462d2e0bc654af2567b933b46df68047ce87dede
George Salu <george.salu10@gmail.com>
2025-01-08 23:14:22 -0300

    demo-security - usando a annotation @repository do spring em PacienteRepository

--- demo-security/src/main/java/com/example/security/repository/PacienteRepository.java
+++ demo-security/src/main/java/com/example/security/repository/PacienteRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.security.domain.Paciente;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
 	@Query("select p from Paciente p where p.usuario.email like :email")
