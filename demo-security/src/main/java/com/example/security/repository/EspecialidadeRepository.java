b4b9c9beaa04f6a343aa16d6192781b9535824c5
George Salu <george.salu10@gmail.com>
2025-01-08 23:13:39 -0300

    demo-security - usando a annotation @repository do spring em EspecialidadeRepository

--- demo-security/src/main/java/com/example/security/repository/EspecialidadeRepository.java
+++ demo-security/src/main/java/com/example/security/repository/EspecialidadeRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.security.domain.Especialidade;
 
+@Repository
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>{
 
 	@Query("select e from Especialidade e where e.titulo like :search%")
