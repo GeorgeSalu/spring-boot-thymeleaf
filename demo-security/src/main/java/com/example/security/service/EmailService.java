f5e457c01c9daf74e91b187a968fda51fec690a8
George Salu <george.salu10@gmail.com>
2025-01-11 14:22:04 -0300

    demo-security - EmailService simula o envio de email

---
+++ demo-security/src/main/java/com/example/security/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.example.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private SpringTemplateEngine template;
+	
+	public void enviarPEdidoDeConfirmacaoDeCadastro(String destino,String codigo) {
+		
+		Context context = new Context();
+		context.setVariable("titulo", "Bem vindo a clinica Spring security");
+		context.setVariable("texto", "Precisamos que confirme seu cadastro, clicando no lnk abaixo");
+		context.setVariable("linkConfirmacao", "http://localhost:8080/u/confirmacao/cadastro?codigo="+codigo);
+		
+		String html = template.process("email/confirmacao", context);
+		
+		System.out.println(html);
+	}
+	
+}
