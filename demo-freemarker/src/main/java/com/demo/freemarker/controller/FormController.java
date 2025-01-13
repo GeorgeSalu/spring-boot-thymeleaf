d84ed46c3cbc2f6917e347a2b6b5b5b87c473dcf
George Salu <george.salu10@gmail.com>
2025-01-13 11:14:23 -0300

    demo-freemarker - enviando dados por formulario

---
+++ demo-freemarker/src/main/java/com/demo/freemarker/controller/FormController.java
@@ -0,0 +1,29 @@
+package com.demo.freemarker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.demo.freemarker.model.User;
+
+@Controller
+public class FormController {
+
+	@GetMapping("/")
+	public String index() {
+		return "redirect:/form";
+	}
+	
+	@GetMapping("/form")
+	public String formGet() {
+		return "form/form";
+	}
+	
+	@PostMapping("/form")
+	public String formPost(User user,Model model) {
+		model.addAttribute("user", user);
+		return "form/form";
+	}
+	
+}
