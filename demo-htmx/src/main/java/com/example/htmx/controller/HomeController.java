43850691d5e4b65df97ec06f56336ec9563ecf53
George Salu <george.salu10@gmail.com>
2025-01-28 11:28:30 -0300

    demo-htmx - ola mundo com htmx

---
+++ demo-htmx/src/main/java/com/example/htmx/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.htmx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@PostMapping("/info")
+	public String info() {
+		return "info";
+	}
+	
+}
