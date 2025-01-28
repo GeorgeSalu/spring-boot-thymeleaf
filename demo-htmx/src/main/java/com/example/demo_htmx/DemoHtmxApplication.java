96d05d87a6313ec3e45ff3fb0a4b9cb9654064df
George Salu <george.salu10@gmail.com>
2025-01-28 10:37:10 -0300

    criacao do projeto demo-htmx

---
+++ demo-htmx/src/main/java/com/example/demo_htmx/DemoHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_htmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHtmxApplication.class, args);
+	}
+
+}
---
+++ demo-htmx/src/test/java/com/example/demo_htmx/DemoHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_htmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
