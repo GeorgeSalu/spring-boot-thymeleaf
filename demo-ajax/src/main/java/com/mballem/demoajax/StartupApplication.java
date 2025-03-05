551457e953b73709a3490123ec2f826c005a2c5a
George Salu <george.salu10@gmail.com>
2025-03-05 11:53:06 -0300

    demo-ajax - renomeia classe de startup

--- demo-ajax/src/main/java/com/mballem/demoajax/StartupApplication.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/StartupApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoAjaxApplication {
+public class StartupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAjaxApplication.class, args);
+		SpringApplication.run(StartupApplication.class, args);
 	}
 }
--- demo-ajax/src/test/java/com/mballem/demoajax/StartupApplicationTests.java
+++ demo-ajax/src/test/java/com/mballem/demoajax/StartupApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoAjaxApplicationTests {
+public class StartupApplicationTests {
 
 	@Test
 	public void contextLoads() {
