961c158f5d52ec064592a84461472c83ff1140a0
George Salu <george.salu10@gmail.com>
2025-03-05 12:42:23 -0300

    demo-ajax - classe de dominio SocialMetaTag

---
+++ demo-ajax/src/main/java/com/mballem/demoajax/domain/SocialMetaTag.java
@@ -0,0 +1,46 @@
+package com.mballem.demoajax.domain;
+
+import java.io.Serializable;
+
+public class SocialMetaTag implements Serializable {
+
+	private static final long serialVersionUID = -492274781922990340L;
+
+	private String site;
+	private String title;
+	private String url;
+	private String image;
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+}
