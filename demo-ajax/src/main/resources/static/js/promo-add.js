f38691efa12b1364dca83540bc40f2567f7c30b8
George Salu <george.salu10@gmail.com>
2025-03-06 11:59:55 -0300

    demo-ajax - incluindo o loading na pagina

--- demo-ajax/src/main/resources/static/js/promo-add.js
+++ demo-ajax/src/main/resources/static/js/promo-add.js
@@ -12,7 +12,8 @@ $("#linkPromocao").on('change', function() {
 				$("#alert").removeClass("alert alert-danger").text("");
 				$("#titulo").val("");
 				$("#site").text("");
-				$("#linkImagem").attr("src", "/images/promo-dark.png");
+				$("#linkImagem").attr("src", "");
+				$("#loader-img").addClass("loader");
 			},
 			success: function(data) {
 				console.log(data);
@@ -23,10 +24,15 @@ $("#linkPromocao").on('change', function() {
 			statusCode: {
 				404: function() {
 					$("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa url");
+					$("#linkImagem").attr("src", "/images/promo-dark.png");
 				}
 			},
 			error: function() {
 				$("#alert").addClass("alert alert-danger").text("Ops.... algo deu errado tente mas tarde");
+				$("#linkImagem").attr("src", "/images/promo-dark.png");
+			},
+			complete: function() {
+				$("#loader-img").removeClass("loader")
 			}
 		})
 		
