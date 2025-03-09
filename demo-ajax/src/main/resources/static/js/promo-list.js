3fc0776010cade6e68c0a74ab58bf8788415f644
George Salu <george.salu10@gmail.com>
2025-03-09 14:30:37 -0300

    demo-ajax - operacao para likes

--- demo-ajax/src/main/resources/static/js/promo-list.js
+++ demo-ajax/src/main/resources/static/js/promo-list.js
@@ -53,3 +53,23 @@ function loadScrollBar(pageNumber) {
 	})
 	
 }
+
+
+// adicionar likes
+$("button[id*='likes-btn-']").on("click", function() {
+	
+	var id = $(this).attr("id").split("-")[2];
+	
+	console.log(id)
+	
+});
+
+
+
+
+
+
+
+
+
+
