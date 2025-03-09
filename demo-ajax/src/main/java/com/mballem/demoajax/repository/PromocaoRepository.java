e52372eea481552893ccb141759e9096228c3952
George Salu <george.salu10@gmail.com>
2025-03-09 15:20:58 -0300

    demo-ajax - o metodo consulta para likes

--- demo-ajax/src/main/java/com/mballem/demoajax/repository/PromocaoRepository.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/repository/PromocaoRepository.java
@@ -14,5 +14,8 @@ public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
 	@Modifying
 	@Query("update Promocao p set p.likes = p.likes + 1 where p.id = :id")
 	void updateSomarLikes(@Param("id") Long id);
-		
+	
+	@Query("select p.likes from Promocao p where p.id = :id")
+	int findLikesById(@Param("id") Long id);
+	
 }
