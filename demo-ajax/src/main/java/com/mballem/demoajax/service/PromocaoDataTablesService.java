2d1ab46aab96b6fc70921cdd4f23af04845a420f
George Salu <george.salu10@gmail.com>
2025-03-13 15:04:27 -0300

    demo-ajax - formatando PromocaoDataTablesService

--- demo-ajax/src/main/java/com/mballem/demoajax/service/PromocaoDataTablesService.java
+++ demo-ajax/src/main/java/com/mballem/demoajax/service/PromocaoDataTablesService.java
@@ -17,57 +17,57 @@ import com.mballem.demoajax.repository.PromocaoRepository;
 
 public class PromocaoDataTablesService {
 
-	private String[] cols = { "id", "titulo", "site", "linkPromocao", "descricao", "linkImagem", "preco", "likes", "dtCadastro", "categoria" };
-	
-	public Map<String, Object> execute(PromocaoRepository repository,HttpServletRequest request) {
-		
+	private String[] cols = { "id", "titulo", "site", "linkPromocao", "descricao", "linkImagem", "preco", "likes",
+			"dtCadastro", "categoria" };
+
+	public Map<String, Object> execute(PromocaoRepository repository, HttpServletRequest request) {
+
 		int start = Integer.parseInt(request.getParameter("start"));
 		int length = Integer.parseInt(request.getParameter("length"));
 		int draw = Integer.parseInt(request.getParameter("draw"));
-		
+
 		int current = currentPage(start, length);
-		
+
 		String column = columnName(request);
 		Sort.Direction direction = orderBy(request);
 		String search = searchBy(request);
-		
+
 		Pageable pageable = PageRequest.of(current, length, direction, column);
-		
+
 		Page<Promocao> page = queryBy(search, repository, pageable);
-		
-		
+
 		Map<String, Object> json = new LinkedHashMap<String, Object>();
 		json.put("draw", draw);
 		json.put("recordsTotal", page.getTotalElements());
 		json.put("recordsFiltered", page.getTotalElements());
 		json.put("data", page.getContent());
-		
+
 		return json;
 	}
-	
+
 	private Page<Promocao> queryBy(String search, PromocaoRepository repository, Pageable pageable) {
-		
-		if(search.isEmpty()) {
+
+		if (search.isEmpty()) {
 			return repository.findAll(pageable);
 		}
-		
-		if(search.matches("^[0-9]+([.,][0-9]{2})")) {
+
+		if (search.matches("^[0-9]+([.,][0-9]{2})")) {
 			search = search.replace(",", ".");
 			return repository.findByPreco(new BigDecimal(search), pageable);
 		}
-		
+
 		return repository.findByTituloOrSiteOrCategoria(search, pageable);
 	}
 
 	private String searchBy(HttpServletRequest request) {
-		
+
 		return request.getParameter("search[value]").isEmpty() ? "" : request.getParameter("search[value]");
 	}
 
 	private Direction orderBy(HttpServletRequest request) {
 		String order = request.getParameter("order[0][dir]");
 		Sort.Direction sort = Sort.Direction.ASC;
-		if(order.equalsIgnoreCase("desc")) {
+		if (order.equalsIgnoreCase("desc")) {
 			sort = Sort.Direction.DESC;
 		}
 		return sort;
@@ -81,5 +81,5 @@ public class PromocaoDataTablesService {
 	private int currentPage(int start, int lenght) {
 		return start / lenght;
 	}
-	
+
 }
