f5f7ef1fc2ca76e16d6636f28f13cfa78b5df42c
George Salu <george.salu10@gmail.com>
2025-03-18 13:01:44 -0300

    demo-htmx - modifica nome de classe que start o projeto

--- demo-htmx/src/main/java/com/example/htmx/Startup.java
+++ demo-htmx/src/main/java/com/example/htmx/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationStartup {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStartup.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
