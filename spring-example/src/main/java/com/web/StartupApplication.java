3001c8d5dc9038cca1dd75d8abf064c3ad68f98d
George Salu <george.salu10@gmail.com>
2025-11-25 09:15:00 -0300

    spring-example - altera classe que inicia a aplicacao

--- spring-example/src/main/java/com/web/StartupApplication.java
+++ spring-example/src/main/java/com/web/StartupApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExampleApplication {
+public class StartupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringExampleApplication.class, args);
+		SpringApplication.run(StartupApplication.class, args);
 	}
 
 }
