b7771dd077fdad6cac8b003c5136749bb88799a8
George Salu <george.salu10@gmail.com>
2025-11-25 21:33:22 -0300

    spring-example - enviando dados do controller para view com modelAndView

--- spring-example/src/main/java/com/web/TestController.java
+++ spring-example/src/main/java/com/web/TestController.java
@@ -3,6 +3,7 @@ package com.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -12,5 +13,12 @@ public class TestController {
 		model.addAttribute("nome", "george");
 		return "teste";
 	}
+
+    @GetMapping("/teste2")
+    public ModelAndView action2() {
+        var modelAndView = new ModelAndView("teste");
+        modelAndView.addObject("nome", "treinaweb");
+        return modelAndView;
+    }
 	
 }
