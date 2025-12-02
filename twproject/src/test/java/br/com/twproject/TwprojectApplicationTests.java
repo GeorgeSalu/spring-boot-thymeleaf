fbc4cd7b595a46fc5056177e0f66b27861b64a58
George Salu <george.salu10@gmail.com>
2025-12-02 09:47:30 -0300

    criando o projeto - twproject

---
+++ twproject/src/test/java/br/com/twproject/TwprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.twproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
