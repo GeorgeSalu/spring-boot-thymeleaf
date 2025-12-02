470de0d790709357eacf4e9a43049e706268dd07
George Salu <george.salu10@gmail.com>
2025-12-02 09:10:27 -0300

    spring-example - entendenco a injecao de dependecias do spring

--- spring-example/src/main/java/com/web/StartupApplication.java
+++ spring-example/src/main/java/com/web/StartupApplication.java
@@ -1,13 +1,26 @@
 package com.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartupApplication {
+public class StartupApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    private final ClientRepository clientRepository;
+
+    public StartupApplication(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(StartupApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(clientRepository);
+        System.out.println("A aplicacao iniciou");
+    }
 }
