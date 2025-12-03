16109afe39fa021de13601ee5fe8cafc46559920
George Salu <george.salu10@gmail.com>
2025-12-03 08:58:41 -0300

    twproject - criando a model e repository de client

---
+++ twproject/src/main/java/br/com/twproject/core/models/Client.java
@@ -0,0 +1,86 @@
+package br.com.twproject.core.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100,nullable = false)
+    private String name;
+
+    @Column(length = 100, unique = true)
+    private String email;
+
+    @Column(length = 11, nullable = false, unique = true)
+    private String phone;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
