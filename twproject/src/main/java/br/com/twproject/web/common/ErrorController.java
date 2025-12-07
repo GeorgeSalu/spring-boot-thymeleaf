c1b454e042c22e111cb641ff8a7bdff4bc526a6b
George Salu <george.salu10@gmail.com>
2025-12-07 15:28:44 -0300

    twproject - tratando erros da aplicacao

---
+++ twproject/src/main/java/br/com/twproject/web/common/ErrorController.java
@@ -0,0 +1,19 @@
+package br.com.twproject.web.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ModelAndView handleNoSuchElementException(NoSuchElementException e) {
+        var model = Map.of("message", e.getMessage());
+        return new ModelAndView("error/error", model);
+    }
+
+}
