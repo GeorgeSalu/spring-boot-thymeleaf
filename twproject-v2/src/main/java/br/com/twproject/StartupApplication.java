7b6cc279b94d12bd562e2e3b53cf2996cec5e55f
George Salu <george.salu10@gmail.com>
2025-12-10 20:50:50 -0300

    twproject-v2 - renomeia classe de teste startup da aplicacao

--- twproject-v2/src/main/java/br/com/twproject/StartupApplication.java
+++ twproject-v2/src/main/java/br/com/twproject/StartupApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwprojectApplication {
+public class StartupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwprojectApplication.class, args);
+		SpringApplication.run(StartupApplication.class, args);
 	}
 
 }
