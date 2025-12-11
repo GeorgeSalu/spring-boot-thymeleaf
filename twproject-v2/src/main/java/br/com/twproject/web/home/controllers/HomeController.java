aa6409149db78f003f041d42603663aaf46abe26
George Salu <george.salu10@gmail.com>
2025-12-11 15:58:58 -0300

    twproject-v2 - formatacao de dados com utility objects

--- twproject-v2/src/main/java/br/com/twproject/web/home/controllers/HomeController.java
+++ twproject-v2/src/main/java/br/com/twproject/web/home/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package br.com.twproject.web.home.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +16,17 @@ public class HomeController {
     @GetMapping()
     public String index() {
         return "home/index";
+    }
+    
+    @GetMapping("/format-data")
+    public ModelAndView formatData() {
+    	var model = Map.of(
+    			"date", LocalDate.of(2024, 10, 10),
+    			"datetime", LocalDateTime.of(2024, 10, 10, 10, 10, 10),
+    			"currency", 1024.51,
+    			"percent", 2.0/100
+    	);
+    	return new ModelAndView("format-data", model);
     }
 
 }
