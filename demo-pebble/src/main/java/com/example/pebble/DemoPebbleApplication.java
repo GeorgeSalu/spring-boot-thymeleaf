eabf03362bd7caf23972e7a3ccd7b3538a448c20
George Salu <george.salu10@gmail.com>
2025-12-21 23:23:42 -0300

    criando projeto demo-pebble

---
+++ demo-pebble/src/main/java/com/example/pebble/DemoPebbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pebble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPebbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPebbleApplication.class, args);
+	}
+
+}
---
+++ demo-pebble/src/test/java/com/example/pebble/DemoPebbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pebble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPebbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
