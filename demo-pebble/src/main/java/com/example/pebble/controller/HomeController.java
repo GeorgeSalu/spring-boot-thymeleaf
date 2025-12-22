9171bb4b117dc87d940917f0485780e897170aca
George Salu <george.salu10@gmail.com>
2025-12-21 23:38:34 -0300

    demo-pebble - tag for

--- demo-pebble/src/main/java/com/example/pebble/controller/HomeController.java
+++ demo-pebble/src/main/java/com/example/pebble/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HomeController {
 
@@ -11,5 +14,12 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("message", "Hello World!");
         return "index";
+    }
+
+    @GetMapping("/nomes")
+    public String mostraNomes(Model model) {
+        List<String> nomes = Arrays.asList("Ana", "Bruno", "Carlos", "Diana");
+        model.addAttribute("nomes", nomes);
+        return "lista-nomes";
     }
 }
